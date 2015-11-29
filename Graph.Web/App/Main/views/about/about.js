d5a8ff54975d7260afe698f71c4de9dc8ad8da2e
Immortall08 <punleon@gmail.com>
2015-11-30 01:09:40 +0300

    Scale-free garph

--- Graph.Web/App/Main/views/about/about.js
+++ Graph.Web/App/Main/views/about/about.js
@@ -8,12 +8,14 @@
             vm.G = new jsnx.Graph();
             vm.n = 50;
             vm.p = 0.1;
+            vm.m = 2;
             vm.optDirected = false;
             
             vm.gnpRandomGraph = gnpRandomGraph;
             vm.fastGnpRandomGraph = fastGnpRandomGraph;
             vm.binomialGraph = binomialGraph;
             vm.erdosRenyiGraph = erdosRenyiGraph;
+            vm.barabasiAlbertGraph = barabasiAlbertGraph;
             vm.cliques;
             vm.numberOfCliques;
 
@@ -21,7 +23,10 @@
             vm.addEdge = false;
             vm.removeNode = false;
             vm.removeEdge = false;
-            
+            vm.draw = draw;
+            vm.g_add_node = g_add_node;
+            vm.degreeHistogram ;
+
             draw();
             
 
@@ -49,14 +54,25 @@
                     },
                     stickyDrag: true
 
-                });
+                }, true);
+                //vm.degreeHistogram = jsnx.degreeHistogram(vm.G);
                 
-                console.log(degree);
+                //console.log(jsnx.toEdgelist (vm.G));
+                
                
+            }
+            function g_add_node() {
+                vm.G.addNode(vm.G.node.size + 1);
+                console.log(vm.G.node.size);
+            }
+            function g_add_edge() {
+                vm.G.addNode(vm.G.node.size + 1);
+                console.log(vm.G.node.size);
             }
 
             function fastGnpRandomGraph(){
                 vm.G = jsnx.fastGnpRandomGraph(vm.n, vm.p, vm.optDirected);
+                console.log(jsnx.degreeHistogram(vm.G));
                 draw();
             };
             function gnpRandomGraph(){
@@ -70,8 +86,88 @@
             function erdosRenyiGraph(){
                 vm.G = jsnx.erdosRenyiGraph(vm.n, vm.p, vm.optDirected);
                 draw();
-            };            
+            };
+            function barabasiAlbertGraph() {
+                vm.G = genBarabasiAlbertGraph(vm.n, vm.m);
+                console.log(jsnx.degreeHistogram(vm.G));
+                draw();
+            };
 
+            function genBarabasiAlbertGraph(n,m){
+                var G = new jsnx.Graph();
+                var edges;
+                var rangeN = range(n);
+                var targets = range(m);
+                var repeatedNodes = [];
+                var source = m;
+                //var sourceArray = [];
+                G.name = 'barabasi_albert_graph('+n+', '+m+')';
+
+                while (source < n) {
+                    //sourceArray = [];
+                    // Add edges to m nodes from the source.
+                    for (var i = 0; i < m; i++) {
+                        G.addEdge(source, targets[i]);
+                        // And the new node "source" has m edges to add to the list.
+                        repeatedNodes.push(source);
+                    };
+                    //G.addEdgesFrom(zip([source] * m, targets));
+                    // Add one node to the list for each new edge just created.
+                    for (var i = 0; i < m; i++) {
+                        repeatedNodes.push(targets[i]);
+                    }
+                    
+                    // Now choose m unique nodes from the existing nodes
+                    // Pick uniformly from repeated_nodes (preferential attachement)
+                    //console.log(targets);
+                    targets = _random_subset(repeatedNodes, m);
+                    //console.log(targets);
+                    source += 1;
+                }                
+                return G;
+
+            };
+
+            function _random_subset(seq, m) {
+                var targets = [];
+                while (targets.length<m){
+                    var x = getRandomInt(0, seq.length-1);
+                    targets.push(seq[x]);
+                }
+                return targets
+            }
+            // использование Math.round() даст неравномерное распределение!
+            
+            function getRandomInt(min, max) {
+                return Math.floor(Math.random() * (max - min + 1)) + min;
+            };
+            function range(optStart, optEnd, optStep) {
+                //console.log(Array.from(genRange(optStart, optEnd, optStep)));
+                return Array.from(genRange(optStart, optEnd, optStep));
+            };
+            function* genRange(optStart, optEnd, optStep) {
+
+                if (optStart == null) {
+                    return;
+                }
+                else if (optEnd == null) {
+                    optEnd = optStart;
+                    optStart = 0;
+                    optStep = 1;
+                }
+                else if (optStep == null) {
+                    optStep = 1;
+                }
+                else if (optStep === 0) {
+                    throw new RangeError("opt_step can't be 0");
+                }
+
+                var negative = optStep < 0;
+                for (var i = optStart; negative && i > optEnd || !negative && i < optEnd; i += optStep) {
+                    yield i;
+                }
+            };
         }
+        
     ]);
 })();
